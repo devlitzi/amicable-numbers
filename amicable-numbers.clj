78d86cbf8645bb9ccfdd071559571d605e81a346
devlitz <z_wolfe@mac.com>
2016-03-08 16:52:34 -0600

    added project

---
+++ amicable-numbers.clj
@@ -0,0 +1,25 @@
+(require '[clojure.core.reducers :as r])
+
+(defn add-factors [n]
+  (- (r/fold + (into (sorted-set)
+    (r/mapcat (fn [x] [x (/ n x)])
+      (r/filter #(zero? (rem n %)) (range 1 (inc (Math/sqrt n))))))) n))
+
+(defn has-amicable-pair? [number]
+  (let [pair (add-factors number)]
+  (and
+    (= (add-factors pair) number)
+    (not= pair number)
+    (> pair number))))
+
+
+(defn find-amicable-numbers [n]
+      (r/foldcat (r/take n (r/filter has-amicable-pair? (range)))))
+
+;;(find-amicable-numbers 5)
+
+(defn find-amicable-pairs [n]
+  (let [x (find-amicable-numbers n)]
+    (map vector x (map add-factors x))))
+
+(time (find-amicable-pairs 20))
